cdda045a79b1e3d44d19bc242cc3ec7923fb8c65
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-09-22 22:42:22 +0300

    add http.go for binding cfg to client

---
+++ internal/downloader/http.go
@@ -0,0 +1,30 @@
+package downloader
+
+import (
+	"net/http"
+	"time"
+)
+
+type HTTPDownloader struct {
+	cfg    Config
+	client *http.Client
+}
+
+func NewHTTPDownloader(cfg Config) *HTTPDownloader {
+	if cfg.ClientTimeout == 0 {
+		cfg.ClientTimeout = time.Second * 60
+	}
+
+	if cfg.DownloadDir == "" {
+		cfg.DownloadDir = "downloads"
+	}
+
+	if cfg.MaxRetries < 0 {
+		cfg.MaxRetries = 0
+	}
+
+	return &HTTPDownloader{
+		cfg:    cfg,
+		client: &http.Client{Timeout: cfg.ClientTimeout},
+	}
+}
