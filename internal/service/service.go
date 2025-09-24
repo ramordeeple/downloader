68a4739f001a10ccded9355e634764c68dd93832
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-09-24 14:40:13 +0300

    creating struct of Service

---
+++ internal/service/service.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"sync"
+	"test-task/internal/downloader"
+	"test-task/internal/models"
+	"test-task/internal/queue"
+	"test-task/internal/store"
+)
+
+type Service struct {
+	mutex sync.Mutex
+	tasks map[string]*models.Task
+	st    store.Store
+	dl    downloader.Downloader
+	q     *queue.Queue
+}
