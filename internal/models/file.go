891cc1028045300998024fe54e90d42ca45ee9ec
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-09-26 15:50:47 +0300

    adding new fields in file model

--- internal/models/file.go
+++ internal/models/file.go
@@ -1,9 +1,13 @@
 package models
 
 type File struct {
-	URL       string     `json:"url"`
-	Name      string     `json:"name"`
-	Status    FileStatus `json:"status"`
-	Error     string     `json:"error,omitempty"`
-	SizeBytes int64      `json:"size_bytes,omitempty"`
+	URL    string     `json:"url"`
+	Name   string     `json:"name"`
+	Status FileStatus `json:"status"`
+	Error  string     `json:"error,omitempty"`
+
+	SizeBytes int64  `json:"size_bytes,omitempty"`
+	BytesDone int64  `json:"bytes_done,omitempty"`
+	ETag      string `json:"etag,omitempty"`
+	LastMod   string `json:"last_mod,omitempty"`
 }
