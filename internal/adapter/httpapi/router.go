124488fec57e1f12201d320ca4f7887b9caacfea
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-01 10:53:27 +0300

    finishing router.go

--- internal/adapter/httpapi/router.go
+++ internal/adapter/httpapi/router.go
@@ -2,19 +2,17 @@ package httpapi
 
 import (
 	"net/http"
+	"test-task/internal/domain"
 )
 
 type TaskUsecase interface {
 	NewTask(urls []string) (string, error)
-	GetTask(id string) (*TaskView, error)
+	GetTask(id string) (*domain.Task, error)
 }
 
 func New(uc TaskUsecase) http.Handler {
 	mux := http.NewServeMux()
-
 	mux.Handle("/tasks", &tasksCreate{uc: uc})
-
 	mux.Handle("/tasks/", &tasksGet{uc: uc})
-
 	return mux
 }
