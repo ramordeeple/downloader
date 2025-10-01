c82260680b62358d7dfc27fed5fe417c3e7a8223
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-01 10:56:46 +0300

    using ports as set of interfaces

--- internal/usecase/ports.go
+++ internal/usecase/ports.go
@@ -1,30 +1,31 @@
 package usecase
 
-import (
-	"context"
-	"test-task/internal/domain"
-	"time"
-)
-
-type TaskRepo interface {
-	SaveTask(*domain.Task) error
-	LoadTask(id string) (*domain.Task, error)
-	LoadAll() (map[string]*domain.Task, error)
-}
-
-type FileFetcher interface {
-	Fetch(ctx context.Context, url, suggestedName, outDir string) (name string, n int64, err error)
-}
-
-type Queue interface {
-	Push(id string)
-	Pop() <-chan string
-	Close()
-}
-
-type Logger interface {
-	Infof(string, ...any)
-	Errorf(string, ...any)
-}
-
-type Clock interface{ Now() time.Time }
+//
+//import (
+//	"context"
+//	"test-task/internal/domain"
+//	"time"
+//)
+//
+//type TaskRepo interface {
+//	SaveTask(*domain.Task) error
+//	LoadTask(id string) (*domain.Task, error)
+//	LoadAll() (map[string]*domain.Task, error)
+//}
+//
+//type FileFetcher interface {
+//	Fetch(ctx context.Context, url, suggestedName, outDir string) (name string, n int64, err error)
+//}
+//
+//type Queue interface {
+//	Push(id string)
+//	Pop() <-chan string
+//	Close()
+//}
+//
+//type Logger interface {
+//	Infof(string, ...any)
+//	Errorf(string, ...any)
+//}
+//
+//type Clock interface{ Now() time.Time }
